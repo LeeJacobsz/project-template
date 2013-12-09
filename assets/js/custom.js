e348eadd3424c53159504aaee6f82ea43106305d
Lee Jacobsz <lee@pitch.net.nz>
2013-12-10 09:40:05 +1300

    added meta name viewport - as all our sites are responsive, also added the bit of code that allows for placeholder text in i.e. - this is to be used with modernizr. However, the latest version of modernizr will be downloaded per project, as will respond.js - which we need for i.e. because we always do mobile first.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,3 +11,52 @@ $(document).ready(function() {
 });
 
 // functions and plugins can go below here for speed, instead of separate and slow script files.
+
+// input placeholder for ie
+// use along with modernizer
+// input elements in html used in same way as if browser supported placeholder text
+jQuery(function () {
+	  // check placeholder browser support
+	  if (!Modernizr.input.placeholder)
+	  {
+			 // set placeholder values
+			 jQuery(this).find('[placeholder]').each(function ()
+			 {
+					 if (jQuery(this).val() == '')  // if field is empty
+					 {
+							jQuery(this).val(jQuery(this).attr('placeholder'));
+							jQuery(this).addClass('placeholder');
+					 }
+			 });
+
+			 // focus and blur of placeholders
+			 jQuery('[placeholder]').focus(function ()
+			 {
+					 if (jQuery(this).val() == jQuery(this).attr('placeholder')) {
+							jQuery(this).val('');
+							jQuery(this).removeClass('placeholder');
+					 }
+			 }
+	  ).blur(function ()
+			 {
+					 if (jQuery(this).val() == '' || jQuery(this).val() == jQuery(this).attr('placeholder'))
+					 {
+					 jQuery(this).val(jQuery(this).attr('placeholder'));
+					 jQuery(this).addClass('placeholder');
+			 }
+	  }
+	  );
+
+			 // remove placeholders on submit
+	  jQuery('[placeholder]').closest('form').submit(function ()
+			 {
+					 jQuery(this).find('[placeholder]').each(function ()
+					 {
+							if (jQuery(this).val() == jQuery(this).attr('placeholder'))
+							{
+								  jQuery(this).val('');
+							}
+					 });
+			 });
+	  }
+});
